3669808149ca845de23c8e7a9c86d3e6cee7b297
Rekiktamrat <rekiktamrat72@gmail.com>
2025-02-10 15:39:10 +0300

    add bulid script

--- src/nav/Navigation.js
+++ src/nav/Navigation.js
@@ -1,19 +1,13 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Login from '../pages/auth/Login';
+
 function App() {
   return (
     <Router>
-      <Navigation /> {/* Add Navigation component here */}
       <Routes>
-        {/* Admin Pages */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="users" element={<Users />} />
-          <Route path="properties" element={<Properties />} />
-          <Route path="settings" element={<Settings />} />
-        </Route>
-
         {/* Other Routes */}
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        
       </Routes>
     </Router>
   );
